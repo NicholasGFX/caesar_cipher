45c2ebb414acbd97afd89d4e0bda23c5cac69dea
NicholasGFX <37931633+NicholasGFX@users.noreply.github.com>
2021-02-28 11:12:23 -0600

    Update caesar.rb

--- caesar.rb
+++ caesar.rb
@@ -6,7 +6,7 @@ def caesar_cipher(string, shift)
   string.map! do |char|
     if alphabet.include? char.downcase # If character is a letter, convert it
       capitalize = true if char != char.downcase # Capitalize true if letter isn't lowercase.
-      new_index = alphabet.find_index(char.downcase) + shift
+      new_index = alphabet.find_index(char.downcase) + shift # Set new index
       new_index -= alphabet.length if new_index > alphabet.length # Fix for if new_index is larger than alphabet array
       capitalize ? alphabet[new_index].upcase : alphabet[new_index] # Return the new letter
     else # Character isn't a letter, so return the original character
